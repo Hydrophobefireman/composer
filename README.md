9534efa2813a6824838a6b61c9d13c5d714d600d
me <bhavesh.pareek36@gmail.com>
2019-11-01 15:10:08 +0530

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+# Composer - Pipe function for Python
+
+## Usage
+
+```python
+from composer import Composer
+def func0(x):
+    return x*2
+
+initial_value = 10
+
+composer  = Composer(func0,initial_value)
+
+def func1(x):
+    return x+5
+
+print(composer.then(func1).value) ##25
+```
+
+alternatively, you can use `|` or `>`
+
+```python
+print((composer > func1).value)
+# Note that you would need to use parenthesis for piping through multiple functions
+# like this
+def func2(x):
+    return x**2
+
+composer = Composer(func0,10)
+result = (composer > func1) > func2
+print(result.value)
+```
+
+You can build a composer without assigning any function by using Composer.identity
+
+```python
+value = 10
+composed = Composer.identity(value)
+```
+
+# Another example
+
+```python
+from composer import Composer
+from requests import get
+from bs4 import BeautifulSoup as Soup
+
+def make_soup(response):
+    return Soup(response,"html.parser")
+
+composer = Composer(get,"https://example.com").then(make_soup)
+soup = composer.value
+
+```
---
+++ composer.py
@@ -0,0 +1,42 @@
+from typing import Callable, Any
+
+
+class Composer(object):
+    def __init__(self, fn: Callable[[Any], Any], *args, **kwargs):
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.value: Any = None
+        self.compute()
+
+    @staticmethod
+    def flatten_composers(c: Composer):
+        d = c
+        while isinstance(d, Composer):
+            d = d.value
+        return d
+
+    def _nextValue(self, c: Callable[[Any], Any]):
+        self.value = c(self.value)
+        return self
+
+    def __or__(self, c: Callable[[Any], Any]):
+        return self._nextValue(c)
+
+    def __gt__(self, c: Callable[[Any], Any]):
+        return self._nextValue(c)
+
+    def compute(self):
+        if self.args:
+            value = self.fn(*self.args, **self.kwargs)
+        else:
+            value = self.fn(**self.kwargs)
+        self.value = Composer.flatten_composers(value)
+
+    def then(self, call):
+        return self._nextValue(call)
+
+    @staticmethod
+    def identity(idx: Any):
+        return Composer(lambda x: x, idx)
+
